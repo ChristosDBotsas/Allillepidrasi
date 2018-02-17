de952f5b4aa0789e4e02628f1791bdd3d1ce6892
ChristosDBotsas <p13098@students.cs.unipi.gr>
2018-02-17 21:21:24 +0200

    Add Homepage Draft

--- Allilepidrasi/Form1.Designer.cs
+++ Allilepidrasi/Form1.Designer.cs
@@ -28,13 +28,50 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.button1.Location = new System.Drawing.Point(102, 86);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 100);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Έλεγχος Θερμοκρασίας";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.button2.Location = new System.Drawing.Point(537, 86);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(200, 100);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Επιλογή ενδυμασίας";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.button3.Location = new System.Drawing.Point(380, 345);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(200, 100);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Έλεγχος δωματίων";
+            this.button3.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Allilepidrasi.Properties.Resources.Steel_Background_Desktop_Wallpaper_14566;
             this.ClientSize = new System.Drawing.Size(1008, 537);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Hello World";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -43,6 +80,10 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
